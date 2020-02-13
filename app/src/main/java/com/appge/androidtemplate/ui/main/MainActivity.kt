b93d6e31f19eb7c5d27d72f64c7eb01e55c51bd7
jh <jhpop@naver.com>
2020-02-13 16:33:41 +0900

    #1 동적권한을 위한 클래스 PermissionHelper

---
+++ app/src/main/java/com/appge/androidtemplate/ui/main/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.appge.androidtemplate.ui.main
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.appge.androidtemplate.R
+import com.appge.androidtemplate.util.PermissionHelper
+import com.appge.androidtemplate.util.checkPermissions
+import com.appge.androidtemplate.util.onRequestPermissionResult
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ *
+ * 1. Manifest 권한 추가
+ * 2. PermissionHelper 필요한 동작에 따른 권한들을 요청코드와 Map 에 정의
+ * 3. 동적 권한이 필요한 동작이 발생하면 권한 체크
+ *    - Activity.checkPermission(requestCode, ...)
+ * 4. 권한이 허용되어 있다면 동작 실행
+ *    - Activity.checkPermission(.., funcAllowed)
+ * 5. 권한이 허용되어 있지 않다면 권한 요청
+ *    - Activity.checkPermission(requestCode, funcAllowed)
+ * 6. 권한 요청에 따른 사용자 반응이 허용이라면 동작 실행
+ *    - Context.onRequestPermissionsResult(.., funcAllowed, ..)
+ * 7. 거부라면 거부에 따른 처리
+ *    - Context.onRequestPermissionsResult(.., funcNotAllowed)
+ *
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        btn_main_camera.setOnClickListener {
+            checkPermissionAndStartCamera()
+        }
+    }
+
+    private fun checkPermissionAndStartCamera() {
+        checkPermissions(PermissionHelper.PERMISSIONS_CAMERA) {
+            startCamera()
+        }
+    }
+
+    private fun startCamera() {
+        Toast.makeText(this, "카메라를 시작합니다 위이잉", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        onRequestPermissionResult(PermissionHelper.PERMISSIONS_CAMERA, {
+            startCamera()
+        }) {
+            showPermissionDeniedMessage()
+        }
+    }
+
+    private fun showPermissionDeniedMessage() {
+        Toast.makeText(this, "권한을 허용하지 않아 카메라를 사용할 수 없습니다.", Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ app/src/main/java/com/appge/androidtemplate/util/PermissionHelper.kt
@@ -0,0 +1,62 @@
+package com.appge.androidtemplate.util
+
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+object PermissionHelper {
+
+    const val PERMISSIONS_CAMERA = 1
+
+    private val permissionList = mapOf(
+        PERMISSIONS_CAMERA to arrayOf(android.Manifest.permission.CAMERA)
+    )
+
+    private fun getPermissions(requestCode: Int) = permissionList[requestCode] ?: throw IllegalArgumentException("You must use requestCode defined at PermissionHelper")
+
+    fun checkPermission(context: Context, requestCode: Int): Boolean {
+        val permissions = getPermissions(requestCode)
+        return checkPermissions(context, permissions)
+    }
+
+    private fun checkPermission(context: Context, permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun checkPermissions(context: Context, permissions: Array<String>): Boolean {
+        return permissions.fold(true, { acc, permission ->
+            acc && checkPermission(context, permission)
+        })
+    }
+
+    fun requestPermissions(activity: Activity, requestCode: Int) {
+        val permissions = getPermissions(requestCode)
+        ActivityCompat.requestPermissions(activity, permissions, requestCode)
+    }
+
+    fun onRequestPermissionResult(context: Context, requestCode: Int, funcAllowed: () -> Unit, funcNotAllowed: () -> Unit = {}) {
+        val permissions = getPermissions(requestCode)
+        if (checkPermissions(context, permissions)) {
+            funcAllowed.invoke()
+        } else {
+            funcNotAllowed.invoke()
+        }
+    }
+}
+
+// PermissionHelper Extensions
+fun Activity.requestPermissions(requestCode: Int) = PermissionHelper.requestPermissions(this, requestCode)
+
+fun Context.checkPermissions(requestCode: Int) = PermissionHelper.checkPermission(this, requestCode)
+
+fun Context.onRequestPermissionResult(requestCode: Int, funcAllowed: () -> Unit, funcNotAllowed: () -> Unit = {}) =
+    PermissionHelper.onRequestPermissionResult(this, requestCode, funcAllowed, funcNotAllowed)
+
+fun Activity.checkPermissions(requestCode: Int, funcAllowed: () -> Unit) {
+    if (this.checkPermissions(requestCode))
+        funcAllowed.invoke()
+    else
+        this.requestPermissions(requestCode)
+}
